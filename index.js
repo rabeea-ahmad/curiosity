2b68bb54603feda4d4c6f4a0558337ceaf59b7e6
Rabeea Ahmad <ra07@hotmail.ca>
2016-12-24 14:15:46 -0500

    Added webook method, and send message function

--- index.js
+++ index.js
@@ -21,3 +21,33 @@ app.get('/webhook', function (req, res) {
     }
 });
 
+app.post('/webhook', function(req, res) {
+	var events = req.body.entry[0].messaging;
+	for (int x = 0; x < events.length; x++) {
+		var event = events[x];
+		if (event.message && event.message.text) {
+			sendMessage(event.sender.id, {text: 'Echo: ' + event.message.text});
+		}
+	}
+	res.sendStatus(200);
+});
+
+function sendMessage(recipientId, text) {
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token: proccess.env.PAGE_ACCESS_TOKEN},
+		method: 'POST',
+		json: {
+			recipient: {id: recipientId},
+			message: message
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("Error sending message: ", error);
+		}
+		else if (response.body.error) {
+			console.log("Error: ", response.body.error);
+		}
+	});
+};
+
