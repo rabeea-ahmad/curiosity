d97e5644ac51e55ab2358c7b5edfff0e7e7c2656
Rabeea Ahmad <ra07@hotmail.ca>
2016-12-24 14:21:22 -0500

    Fixed typo

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/webhook', function(req, res) {
 function sendMessage(recipientId, text) {
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token: proccess.env.PAGE_ACCESS_TOKEN},
+		qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
 		method: 'POST',
 		json: {
 			recipient: {id: recipientId},
